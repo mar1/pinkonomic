9e51ff6004371ea972e1c930500050e69d978b35
Kartheek, Gottipati <15101965+kartheekgottipati@users.noreply.github.com>
2025-03-13 22:50:01 -0400

    remove logging

--- app/components/buttons/SwapButton.tsx
+++ app/components/buttons/SwapButton.tsx
@@ -127,9 +127,6 @@ const SwapButton = () => {
     "loadPreviousStateFromLocalStorage": true
   };
 
-  console.log("Integrator ID:", import.meta.env.VITE_SQUID_INTEGRATOR_ID); // Debug log
-  console.log("Squid Config:", squidConfig);
-  
   // Construct the iframe src dynamically
   const iframeSrc = `https://studio.squidrouter.com/iframe?config=${encodeURIComponent(JSON.stringify(squidConfig))}`;
 
